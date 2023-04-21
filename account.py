f238d6ec7a698220f6a16c4f56859f39012cd2aa
Keaghonc <keaghonc@gmail.com>
2023-04-21 14:09:38 -0500

    Code for account file

--- account.py
+++ account.py
@@ -1,47 +1,25 @@
 class Account:
 
-    def __init__(self, name: str) -> None:
-        """
-        Function to set up account object.
-        :param name: Account name
-        """
+    def __init__(self, name: str):
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount: float) -> bool:
-        """
-        Allows user to deposit an amount into the account.
-        :param amount: Desired user amount.
-        :return: True or False depending on if process was successful.
-        """
+    def deposit(self, amount: float):
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount: float) -> bool:
-        """
-        Allows user to withdraw an amount from the account.
-        :param amount: Desired user amount.
-        :return: True or False depending on if process was successful.
-        """
+    def withdraw(self, amount: float):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self) -> float:
-        """
-        Allows user to view balance.
-        :return: User balance.
-        """
+    def get_balance(self):
         return self.__account_balance
 
-    def get_name(self) -> str:
-        """
-        Allows user to view account name.
-        :return: Account name.
-        """
+    def get_name(self):
         return self.__account_name
